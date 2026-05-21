f5dbb6e717379b998177890f2847185cca7edb46
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-21 16:57:39 +0530

    Update s3.ts

--- fitmeals/Web/src/config/s3.ts
+++ fitmeals/Web/src/config/s3.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export const s3 = new S3Client({
   region: process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.MY_AWS_SECRET_KEY!,
   },
 });
 
