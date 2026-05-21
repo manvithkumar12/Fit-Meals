90403967bd36acdaefe3b5456cc00f5d406d31df
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-21 17:05:07 +0530

    Enable bundle analyzer in Next.js configuration

--- fitmeals/Web/next.config.ts
+++ fitmeals/Web/next.config.ts
@@ -2,6 +2,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
