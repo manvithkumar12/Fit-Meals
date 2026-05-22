52f69c7117a585ca61e1ae49e020043757e6c8e8
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-22 11:50:47 +0530

    Update CookBook-item.tsx

--- fitmeals/Web/src/Components/ServiceComponent/CookBook/CookBook-item.tsx
+++ fitmeals/Web/src/Components/ServiceComponent/CookBook/CookBook-item.tsx
@@ -4,6 +4,7 @@ import HeroSection from "../../FoodComponents/HeroSection";
 import Ingredients from "../../IngredientSection/Ingredients";
 import NutritionalComponent from "../../NutritionalCard/NutritionalComponent";
 import Steps from "../../Preparation/Steps";
+
 interface Cookbookprops {
   Item: {
     id: number;
@@ -21,101 +22,72 @@ interface Cookbookprops {
     fatsPer100gm: number;
     carboHydratePer100gm: number;
     mainurl: string;
+    FoodIngredients?: {
+      id?: number;
+      title: string;
+      quantity: string;
+      imgUrl: string;
+    }[];
   };
 }
+
 const CookBookItem = ({ Item }: Cookbookprops) => {
+  const ingredients = Item.FoodIngredients || [];
+
   return (
     <>
       <ChatBotButton />
-      <div className="pb-5 flex flex-col overflow-hidden justify-center items-center z-5">
-        <div className="w-screen flex pt-10 flex-col lg:flex-row  lg:pl-10 ">
-          <HeroSection
-            title={Item.title}
-            description={Item.description}
-            calories={Item.calories}
-            time={Item.time}
-            weight={Item.weight}
-            ImgUrl={Item.mainurl}
-            Rating={Item.nutritionalValue}
-          />
-        </div>
-        <div className="h-max w-[98%] flex-col  pl-0 lg:items-start  lg:flex-row flex gap-5 md:gap-10 pt-5 lg:pl-15 lg:pr-10 md:pr-5 justify-center items-center ">
-          <div className="h-max w-[90%] xl:w-[70%] lg:w-[50%] gap-5 flex flex-col">
-            <Ingredients
-              IngredientList={[
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-              ]}
-            />
-            <Ingredients
-              IngredientList={[
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-                {
-                  title: "Chicken",
-                  weight: 300,
-                  imgUrl:
-                    "https://drin721riupcf.cloudfront.net/cookbook/chicken.webp",
-                },
-              ]}
-            />
-          </div>
-          <div className="h-max p-5 w-[90%]  lg:mr-0 lg:ml-0 lg:w-[80%] max-w-max pr-10 rounded-2xl shadow-lg">
-            <NutritionalComponent
-              EnergyKcal={23}
-              Fat={10}
-              Salt={4}
-              Carbohydrates={150}
-              Protein={25}
+
+      <div className="min-h-screen bg-zinc-50/40 dark:bg-zinc-950 pb-20">
+
+        <div className="w-full max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-8">
+          <div className="w-full flex flex-col lg:flex-row gap-8 bg-white dark:bg-zinc-900 rounded-3xl p-6 md:p-8 border border-zinc-100 dark:border-zinc-800/80 shadow-xl shadow-zinc-100/30 dark:shadow-none">
+            <HeroSection
+              title={Item.title}
+              description={Item.description}
+              calories={Item.calories}
+              time={Item.time}
+              weight={Item.weight}
+              ImgUrl={Item.mainurl}
+              Rating={Item.nutritionalValue}
             />
           </div>
         </div>
-        <div className="w-[95%]">
-          <div className="w-[98%] bg-[#fbf9f3] ml-auto mr-auto lg:w-[95%]   mt-7 rounded-2xl shadow-lg">
+
+
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
+            
+
+            <div className="lg:col-span-2 flex flex-col gap-8">
+
+              <Ingredients IngredientList={ingredients} />
+            </div>
+
+
+            <div className="lg:col-span-1 lg:sticky lg:top-8">
+              <div className="bg-white dark:bg-zinc-900 border border-zinc-100 dark:border-zinc-800/80 rounded-3xl p-6 shadow-xl shadow-zinc-100/30 dark:shadow-none transition-all duration-300">
+                <NutritionalComponent
+                  EnergyKcal={Item.caloriesPer100gm}
+                  Fat={Item.fatsPer100gm}
+                  Salt={0}
+                  Carbohydrates={Item.carboHydratePer100gm}
+                  Protein={Item.proteinPer100gm}
+                />
+              </div>
+            </div>
+            
+          </div>
+
+
+          <div className="mt-8 bg-white dark:bg-zinc-900 border border-zinc-100 dark:border-zinc-800/80 rounded-3xl p-6 md:p-8 shadow-xl shadow-zinc-100/30 dark:shadow-none transition-all duration-300">
             <Steps
               images={Item.imgUrl}
               steps={Item.steps}
             />
           </div>
         </div>
+
       </div>
     </>
   );
