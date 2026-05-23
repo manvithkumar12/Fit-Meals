a26d7276bb3915b73a08dee2fd313f20fe68681f
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 11:35:00 +0530

    Add useReservations hook for fetching reservations

---
+++ fitmeals/Web/src/query/getReservations.ts
@@ -0,0 +1,13 @@
+import { getResResvertations } from "@/app/api/actions/Reservations/getResReservation";
+import { useQuery } from "@tanstack/react-query";
+
+export const useReservations = (restaurantId: number) => {
+    return useQuery({
+        queryKey: ["reservations", restaurantId],
+        queryFn: () => getResResvertations(restaurantId),
+        refetchOnWindowFocus: false,
+        refetchInterval: 3000,
+        refetchIntervalInBackground: true,
+        staleTime: 4000,
+    });
+}
