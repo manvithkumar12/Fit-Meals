60e5c355e6d933ebb3b65d00edd58edf1713805b
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 11:57:37 +0530

    Refactor getRestaurantById to use type and return value

--- fitmeals/Web/app/api/actions/Reservations/getRestaurants.ts
+++ fitmeals/Web/app/api/actions/Reservations/getRestaurants.ts
@@ -1,15 +1,33 @@
 "use server";
+
+import { Prisma } from "@prisma/client";
 import { prisma } from "@/src/lib/prisma";
 
-export const getRestaurantById = async (id: number) => {
+export type RestaurantById =
+  Prisma.RestaurantGetPayload<{
+    select: {
+      id: true;
+      openingTime: true;
+      closingTime: true;
+      reservation: true;
+    };
+  }>;
+
+export const getRestaurantById = async (
+  id: number
+): Promise<RestaurantById | null> => {
   try {
-    const res= await prisma.restaurant.findUnique({
+    return await prisma.restaurant.findUnique({
       where: { id },
+      select: {
+        id: true,
+        openingTime: true,
+        closingTime: true,
+        reservation: true,
+      },
     });
-    return res;
   } catch (err) {
     console.log(err);
     return null;
   }
 };
-
