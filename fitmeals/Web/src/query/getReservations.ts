30499792aa546301842a9ac3075b93c2467fb2f9
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 11:54:36 +0530

    Update getReservations.ts

--- fitmeals/Web/src/query/getReservations.ts
+++ fitmeals/Web/src/query/getReservations.ts
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 export const useReservations = (restaurantId: number) => {
     return useQuery({
         queryKey: ["reservations", restaurantId],
-        queryFn: () => getResResvertations(restaurantId),
+        queryFn: () => getResReservation(restaurantId),
         refetchOnWindowFocus: false,
         refetchInterval: 3000,
         refetchIntervalInBackground: true,
