7a63076b82dcb94c54678894530de9c9948da3ec
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 12:02:35 +0530

    Update getRestaurants.ts

--- fitmeals/Web/app/api/actions/Reservations/getRestaurants.ts
+++ fitmeals/Web/app/api/actions/Reservations/getRestaurants.ts
@@ -7,9 +7,34 @@ export type RestaurantById =
   Prisma.RestaurantGetPayload<{
     select: {
       id: true;
+      name: true;
+      images: true;
+      ownerId: true;
+      cuisineType: true;
+      priceForTwo: true;
+      pinCode: true;
+      facilities: true;
+      description: true;
       openingTime: true;
-      closingTime: true;
       reservation: true;
+      menuUrls: true;
+      resUrls: true;
+      reservationTime: true;
+      closingTime: true;
+      mapLink: true;
+      phoneNumber: true;
+      area: true;
+      averageRating: true;
+      city: true;
+      houseNo: true;
+      lat: true;
+      long: true;
+      status: true;
+      agreementUrl: true;
+      streetName: true;
+      totalReviews: true;
+      totalPersons: true;
+      address: true;
     };
   }>;
 
@@ -21,9 +46,34 @@ export const getRestaurantById = async (
       where: { id },
       select: {
         id: true,
+        name: true,
+        images: true,
+        ownerId: true,
+        cuisineType: true,
+        priceForTwo: true,
+        pinCode: true,
+        facilities: true,
+        description: true,
         openingTime: true,
-        closingTime: true,
         reservation: true,
+        menuUrls: true,
+        resUrls: true,
+        reservationTime: true,
+        closingTime: true,
+        mapLink: true,
+        phoneNumber: true,
+        area: true,
+        averageRating: true,
+        city: true,
+        houseNo: true,
+        lat: true,
+        long: true,
+        status: true,
+        agreementUrl: true,
+        streetName: true,
+        totalReviews: true,
+        totalPersons: true,
+        address: true,
       },
     });
   } catch (err) {
