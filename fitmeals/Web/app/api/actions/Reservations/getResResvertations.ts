822ceb40711702fe8e37ab2a86df3bb8b7d3c950
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 11:33:54 +0530

    Create getResResvertations.ts

---
+++ fitmeals/Web/app/api/actions/Reservations/getResResvertations.ts
@@ -0,0 +1,21 @@
+"use server"
+import { prisma } from "@/src/lib/prisma"
+
+export const getResResvertations = async (restaurantId: number) => {
+    try {
+        const res = await prisma.reservations.findMany({
+            where: { restaurantId: restaurantId },
+            select: {
+                id: true,
+                reservationTime: true,
+                numberOfPeople: true,
+                customerName: true,
+                reservationDate: true,
+            }
+        });
+        return res;
+    } catch (err) {
+        console.log(err);
+        throw new Error("unable to get data");
+    }
+}
