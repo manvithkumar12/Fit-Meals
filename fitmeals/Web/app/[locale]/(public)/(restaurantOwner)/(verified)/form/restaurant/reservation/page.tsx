1c85e21cd6974c9d1f48688321f6a71fbb034304
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-23 11:35:43 +0530

    Implement error handling and update reservation page layout
    Added error handling for missing restaurant data and replaced SlotsCard with NavigatorPage.

--- fitmeals/Web/app/[locale]/(public)/(restaurantOwner)/(verified)/form/restaurant/reservation/page.tsx
+++ fitmeals/Web/app/[locale]/(public)/(restaurantOwner)/(verified)/form/restaurant/reservation/page.tsx
@@ -1,10 +1,10 @@
 import { getRestaurantById } from "@/app/api/actions/Reservations/getRestaurants";
 import { getRestaurantId } from "@/app/api/actions/restaurant/getRestaurantId";
 import { getUser } from "@/lib/CurrentUser";
-import SlotsCard from "@/src/Components/SlotCards/SlotsCard";
 import { getTranslations } from "next-intl/server";
-import Image from "next/image";
 import React from "react";
+import NavigatorPage from "./Navigator";
+import ErrorComponent from "@/src/Components/errorComponent/ErrorComponent";
 
 const page = async ({
   params,
@@ -19,26 +19,12 @@ const page = async ({
   const user = await getUser();
   const id = await getRestaurantId(user?.id!);
   const restaurantData = await getRestaurantById(id?.id!);
+  if (!restaurantData) {
+    return <div className="h-90 w-90 md:h-120 md:w-120"><ErrorComponent whiteBg label="Please try again later" refreshBtn /></div>;
+  }
   return (
-    <div className="w-full flex justify-center">
-      <div className="lg:w-[70%] w-[90%] flex-col items-center flex pt-3 h-max">
-        <div className="flex justify-center w-full items-center">
-          <div className="lg:h-25 lg:w-25 md:w-15 md:h-15 h-10 w-10 relative">
-            <Image
-              src="https://img.icons8.com/arcade/128/alarm.png"
-              fill
-              alt="bell_icon"
-              sizes="(min-width: 1024px) 100px, (min-width: 768px) 60px, 40px"
-            />
-          </div>
-          <h1 className="text-lg md:text-2xl font-semibold">{t("title")}</h1>
-        </div>
-        <SlotsCard
-          id={restaurantData?.id ?? 0}
-          openingTime={restaurantData?.openingTime ?? "00:00"}
-          closingTime={restaurantData?.closingTime ?? "00:00"}
-        />
-      </div>
+    <div className="w-full min-h-screen bg-slate-50/20 py-8 px-4 md:px-6 flex justify-center items-start">
+      <NavigatorPage restaurantData={restaurantData} />
     </div>
   );
 };
