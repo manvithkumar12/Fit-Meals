bfdd2b3a2f412b38f44176c2e2150486d15e4465
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-08-23 09:39:00 +0530

    Update page.tsx

--- fitmeals/Web/app/[locale]/(public)/(customer)/services/order/[pageNo]/[restaurantId]/(restaurantspages)/page.tsx
+++ fitmeals/Web/app/[locale]/(public)/(customer)/services/order/[pageNo]/[restaurantId]/(restaurantspages)/page.tsx
@@ -1,8 +1,9 @@
 import { getUser } from "@/lib/CurrentUser";
 import HotelFilters from "@/src/Components/FlexFilter/HotelFilters";
 import { cartRestaurantId } from "@/src/models/user/cartModel";
-import React from "react";
+import React, { Suspense } from "react";
 import FoodItems from "./FoodItems";
+import FoodBoxLoading from "@/src/Components/ServiceComponent/order/FoodBoxLoading";
 
 const Page = async ({
   params,
@@ -16,17 +17,30 @@ const Page = async ({
   return (
     <div className="flex flex-col lg:flex-row w-full">
       <div className="w-full lg:w-[30%] h-max p-2 lg:pt-10">
-        <HotelFilters />
+        <Suspense fallback={<div className="w-full h-96 bg-white rounded-lg animate-pulse border border-gray-200" />}>
+          <HotelFilters />
+        </Suspense>
       </div>
-      <div className="lg:w-[70%] h-max">
-        <FoodItems
-          restaurantId={RNumber}
-          RestaurantID={cartRestaurant?.restaurantId}
-          cartItems={cartRestaurant?.cartItems}
-        />
+      <div className="w-full lg:w-[70%] h-max">
+        <Suspense
+          fallback={
+            <div className="w-full grid grid-cols-1 gap-2 lg:grid-cols-2 lg:pt-10 h-full p-2">
+              {Array.from({ length: 4 }).map((_, index) => (
+                <FoodBoxLoading key={index + 1} />
+              ))}
+            </div>
+          }
+        >
+          <FoodItems
+            restaurantId={RNumber}
+            RestaurantID={cartRestaurant?.restaurantId}
+            cartItems={cartRestaurant?.cartItems}
+          />
+        </Suspense>
       </div>
     </div>
   );
 };
 
 export default Page;
+
